3410d330ad78274de6f6a0b3d498f7e429147717
Felipe David <felipe.besorean@hotmail.com>
2021-08-28 21:35:53 -0300

    exercicio6

---
+++ trybe-exercicios/fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2/exercicio6.js
@@ -0,0 +1,13 @@
+/**
+ * Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+ */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numberosImpares = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 != 0) {
+    numberosImpares ++;
+  }
+}
+console.log(numberosImpares)
